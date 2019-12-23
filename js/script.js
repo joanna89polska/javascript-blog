5cabaabe30e6271ddd61760641cb408cb54ad5e2
Joanna Polska <joanna19polska@gmail.com>
2019-12-23 01:59:16 +0100

    changes in Authors

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@
 
     /* [DONE] remove class 'active' from all articles */
 
-    const activeArticles = document.querySelectorAll('.posts article.active');
+    const activeArticles = document.querySelectorAll('.posts .active');
 
     for (let activeArticle of activeArticles) {
       activeArticle.classList.remove('active');
@@ -183,7 +183,7 @@
 
     /* make a new constant "tag" and extract tag from the "href" constant */
 
-    const tag = href.replace('#tag-', '');
+    const tag = href;
 
     /* find all tag links with class active */
 
